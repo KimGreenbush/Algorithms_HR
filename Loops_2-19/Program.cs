d26ae8c4f99d2da8b07248e6154847c51aea1ec9
Kim Greenbush <kim_greenbush@icloud.com>
2021-02-19 23:42:00 -0800

    Loops, HR 30DoC, day5

--- Loops_2-19/Program.cs
+++ Loops_2-19/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace Loops_2_19
+class Solution
 {
-    class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int total = 0;
+        for (int i = 1; i <= 10; i++)
         {
-            Console.WriteLine("Hello World!");
+            total = i * n;
+            Console.WriteLine($"{n} x {i} = {total}");
         }
     }
 }
