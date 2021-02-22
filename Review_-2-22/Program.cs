53c26e6d8224e2cc1908734aa05535c7c6a5130b
Kim Greenbush <kim_greenbush@icloud.com>
2021-02-22 12:59:40 -0800

    Loop/string review, HR 30DoC

---
+++ Review_-2-22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string[] words = args;
+        string word = Console.ReadLine();
+        string FirstHalf = "";
+        string SecHalf = "";
+        int i = 0;
+        while (i < word.Length)
+        {
+            if (i % 2 == 0)
+            {
+                FirstHalf += word[i];
+            }
+            else
+            {
+                SecHalf += word[i];
+            }
+            i++;
+        }
+        Console.WriteLine($"{FirstHalf} {SecHalf}");
+    }
+}
