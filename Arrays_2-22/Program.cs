523fa2ac2055072171b4fc3f6f5780723d638dea
Kim Greenbush <kim_greenbush@icloud.com>
2021-02-22 16:18:07 -0800

    Arrays, HR 30Doc

---
+++ Arrays_2-22/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        string ReverseWords = "";
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            ReverseWords += arr[i] + " ";
+        }
+        ReverseWords += arr[0];
+        Console.WriteLine(ReverseWords);
+    }
+}
