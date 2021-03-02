6ba4fc90db6eba3e144299cd82ec860151222ad3
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-02 00:50:46 -0800

    Scope, HR 30Doc

---
+++ Scope_2-28/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] __elements)
+    {
+        elements = __elements;
+    }
+
+    public void computeDifference()
+    {
+        int max = elements[0];
+        int min = elements[0];
+
+        foreach (int element in elements)
+        {
+            if (element < min)
+            {
+                min = element;
+            }
+            if (element > max)
+            {
+                max = element;
+            }
+        }
+        maximumDifference = max - min;
+    }
+
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
