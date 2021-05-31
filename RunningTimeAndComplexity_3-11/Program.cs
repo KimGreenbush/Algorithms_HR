fb80214820fb2c8fe5f240b339395080da8f2de0
Kim Greenbush <kim_greenbush@icloud.com>
2021-05-30 18:39:46 -0700

    HR30DoC, RunningTimeAndComplexity

---
+++ RunningTimeAndComplexity_3-11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int T = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int j = 2;
+            bool prime = true;
+
+            if (n <= 1)
+            {
+                prime = false;
+            }
+
+            while (j*j > n)
+            {
+                if (n%j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+                j++;
+            }
+
+            if (prime == true)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not prime");
+            }
+        }
+    }
+}
