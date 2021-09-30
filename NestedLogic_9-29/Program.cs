6a9f31a3e4ba89cbca249e89821dc4e9e0240baa
Kimberley Greenbush <v-kgreenbush@microsoft.com>
2021-09-30 10:46:37 -0700

    add ReadLine for input, conversion func to compare

--- NestedLogic_9-29/Program.cs
+++ NestedLogic_9-29/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace NestedLogig
+class Solution
 {
-    class Program
+
+    static Func<string[], int[]> ConvertToIntArray = array =>
     {
-        static void Main(string[] args)
+        int[] newArray = new int[3];
+
+        for (int i = 0; i < array.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+            newArray[i] = Convert.ToInt32(array[i]);
         }
+
+        return newArray;
+    };
+
+    static void Main(String[] args)
+    {
+        string[] returnedDate = Console.ReadLine().Split(" ");
+        string[] dueDate = Console.ReadLine().Split(" ");
+
+        int[] intDate = ConvertToIntArray(returnedDate);
+
+        //code
+
+        // is it late?
+        // no,  return date <= due date
+        // yes, return date > due date
+
+        // not late no fine! fine = 0
+
+        // late, calculate fine
+        // days, 15 hackos * days
+        // months, 500 hackos * month
+        // years, 10000 hackos fixed
+
     }
 }
