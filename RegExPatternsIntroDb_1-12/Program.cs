745199800a69918effdb84cf4cd89a02940fbece
KimGreenbush <kimgreenbush@gmail.com>
2022-01-11 22:20:06 -0800

    initial day28

---
+++ RegExPatternsIntroDb_1-12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RegExPatternsIntroDb_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                string firstName = firstMultipleInput[0];
+
+                string emailID = firstMultipleInput[1];
+            }
+        }
+    }
+}
