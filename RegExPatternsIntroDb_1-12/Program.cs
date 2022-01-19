d8af9c71d355157b257e1ec3967e5e9dc6af30fd
KimGreenbush <kimgreenbush@gmail.com>
2022-01-18 23:08:03 -0800

    match using regex and sort for display

--- RegExPatternsIntroDb_1-12/Program.cs
+++ RegExPatternsIntroDb_1-12/Program.cs
@@ -1,21 +1,45 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace RegExPatternsIntroDb_1_12
+class Solution
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Regex gmail = new Regex("[a-z]+@gmail.com");
+
+        List<KeyValuePair<string, string>> emails = new List<KeyValuePair<string, string>>();
+
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int NItr = 0; NItr < N; NItr++)
         {
-            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            for (int NItr = 0; NItr < N; NItr++)
+            string firstName = firstMultipleInput[0];
+
+            string emailID = firstMultipleInput[1];
+
+            if (gmail.IsMatch(emailID))
             {
-                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
-
-                string firstName = firstMultipleInput[0];
-
-                string emailID = firstMultipleInput[1];
+                KeyValuePair<string, string> kvp = new KeyValuePair<string, string>(firstName, emailID);
+                emails.Add(kvp);
             }
+        }
+        var sortedEmails = emails.OrderBy(email => email.Key);
+        foreach (KeyValuePair<string, string> email in sortedEmails)
+        {
+            Console.WriteLine(email.Key);
         }
     }
 }
